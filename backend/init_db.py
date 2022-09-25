4c1e8941abfac90e8971ae90df16fda58985432e
David <david_ristov@live.com>
2022-09-25 15:33:16 +0200

    add frontend, backend changes

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,13 +1,18 @@
-from http import client
+# from http import client
+import re
 from SPARQLWrapper import SPARQLWrapper, JSON
 from pymongo import MongoClient
+import urllib.request
+
 
 client = MongoClient('localhost', 27017)
 db = client["hiphopdb"]
-collection = db["hiphopcol"]
+artists_collection = db["artists"]
+records_collection = db["labels"]
  
-artists = ["Eminem", "Akon"]
-id = 1
+artists = ["Eminem", "Akon", "50_Cent"]
+records = ["Aftermath_Entertainment", "Death_Row_Records"]
+id = 1 # used for primary key, tmp solution
 
 for artist in artists:
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -21,7 +26,9 @@ for artist in artists:
         WHERE {
             dbr:%s dbo:abstract ?abstract ;
                 dbo:birthDate ?birthDate ;
-                dbo:birthPlace ?birthPlace .
+                dbo:birthPlace ?birthPlace ;
+                dbo:thumbnail ?thumbnail .
+            FILTER(lang(?abstract)="en")
         }
         """ % (artist)
     )
@@ -29,14 +36,56 @@ for artist in artists:
     ret = sparql.queryAndConvert()
 
     for r in ret["results"]["bindings"]:
-        if(r["abstract"]["xml:lang"] == "en"):
-            dict = {
-                "_id": id,
-                "name": artist,
-                "birthDate": r["birthDate"]["value"],
-                "birthPlace": r["birthPlace"]["value"],
-                "abstract": r["abstract"]["value"],
+        dict = {
+            "_id": id,
+            "name": artist,
+            "birthDate": r["birthDate"]["value"],
+            "birthPlace": r["birthPlace"]["value"],
+            "abstract": r["abstract"]["value"],
+            "thumbnail": r["thumbnail"]["value"]
             }
-            id += 1
-            # collection.insert_one(dict)
-            print(dict)
+            # image = r["thumbnail"]["value"]
+            # print(image)
+            # urllib.request.urlretrieve(image, "./frontend/" + dict["name"] + ".jpg")
+        id += 1
+        artists_collection.insert_one(dict)
+
+id = 0
+
+for record in records:
+    sparql.setQuery("""
+        PREFIX dbp: <http://dbpedia.org/resource/>
+        PREFIX dbr: <http://dbpedia.org/resource/>
+        PREFIX dbp: <http://dbpedia.org/property/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT *
+        WHERE {
+            dbr:%s dbo:abstract ?abstract ;
+                   dbo:location ?location ;
+                   dbp:country ?country .
+            FILTER(lang(?abstract)="en")     
+        }
+        """ % (record)
+    )
+    sparql.setReturnFormat(JSON)
+    ret = sparql.queryAndConvert()
+
+    location = []
+    abstract = []
+    country = []
+    for r in ret["results"]["bindings"]:
+        location.append(r["location"]["value"])
+        abstract.append(r["abstract"]["value"])
+        country.append(r["country"]["value"])
+
+    dict = {
+        "_id": id,
+        "name": record,
+        "abstract": abstract,
+        "location": location,
+        "country": country
+    }
+    records_collection.insert_one(dict)
+    id += 1
+    
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from http import client
-from SPARQLWrapper import SPARQLWrapper, JSON
 from pymongo import MongoClient
 from fastapi import FastAPI
 
@@ -7,8 +6,14 @@ app = FastAPI()
 
 client = MongoClient('localhost', 27017)
 db = client["hiphopdb"]
-collection = db["hiphopcol"]
+artists_collection = db["artists"]
+records_collection = db["labels"]
 
 @app.get("/api/artist/{name}")
-async def getArtist(name: str):
-    return collection.find_one({"name": name})
+async def getArtistByName(name: str):
+    return artists_collection.find_one({"name": name})
+
+
+@app.get("/api/records/{name}")
+async def getRecordByName(name: str):
+    return records_collection.find_one({"name": name})
